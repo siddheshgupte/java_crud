248789aec95c891908dc7485760a0d20a86c6540
Siddhesh <sgupte@fulcrumgt.com>
2020-04-29 17:06:18 -0700

    Simplify columns

--- demo/src/main/java/com/example/demo/Model/User.java
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -27,24 +27,6 @@ public class User {
         @Column(name = "email_address", nullable = false)
         private String email;
 
-        @CreationTimestamp
-        @Temporal(TemporalType.TIMESTAMP)
-        @Column(name = "created_at", nullable = false)
-        private Date createdAt;
-
-        @Column(name = "created_by", nullable = false)
-        @CreatedBy
-        private String createdBy;
-
-        @UpdateTimestamp
-        @Temporal(TemporalType.TIMESTAMP)
-        @Column(name = "updated_at", nullable = false)
-        private Date updatedAt;
-
-        @Column(name = "updated_by", nullable = false)
-        @LastModifiedBy
-        private String updatedBy;
-
         /**
          * Gets id.
          *
@@ -117,77 +99,6 @@ public class User {
             this.email = email;
         }
 
-        /**
-         * Gets created at.
-         *
-         * @return the created at
-         */
-        public Date getCreatedAt() {
-            return createdAt;
-        }
-
-        /**
-         * Sets created at.
-         *
-         * @param createdAt the created at
-         */
-        public void setCreatedAt(Date createdAt) {
-            this.createdAt = createdAt;
-        }
-
-        /**
-         * Gets created by.
-         *
-         * @return the created by
-         */
-        public String getCreatedBy() {
-            return createdBy;
-        }
-
-        /**
-         * Sets created by.
-         *
-         * @param createdBy the created by
-         */
-        public void setCreatedBy(String createdBy) {
-            this.createdBy = createdBy;
-        }
-
-        /**
-         * Gets updated at.
-         *
-         * @return the updated at
-         */
-        public Date getUpdatedAt() {
-            return updatedAt;
-        }
-
-        /**
-         * Sets updated at.
-         *
-         * @param updatedAt the updated at
-         */
-        public void setUpdatedAt(Date updatedAt) {
-            this.updatedAt = updatedAt;
-        }
-
-        /**
-         * Gets updated by.
-         *
-         * @return the updated by
-         */
-        public String getUpdatedBy() {
-            return updatedBy;
-        }
-
-        /**
-         * Sets updated by.
-         *
-         * @param updatedBy the updated by
-         */
-        public void setUpdatedBy(String updatedBy) {
-            this.updatedBy = updatedBy;
-        }
 
         @Override
         public String toString() {
@@ -196,10 +107,6 @@ public class User {
                     ", firstName='" + firstName + '\'' +
                     ", lastName='" + lastName + '\'' +
                     ", email='" + email + '\'' +
-                    ", createdAt=" + createdAt +
-                    ", createdBy='" + createdBy + '\'' +
-                    ", updatedAt=" + updatedAt +
-                    ", updatedby='" + updatedBy + '\'' +
                     '}';
         }
     }
